f41236fccc3933f70d2369dc48ebbc15854d5b4e
23greg23 <g.grinnell@neu.edu>
2021-08-30 17:43:41 -0400

    working on "fly in" scenarios

--- script.js
+++ script.js
@@ -2,12 +2,24 @@
 gsap.registerPlugin(ScrollTrigger);
 
 // You can use a ScrollTrigger in a tween or timeline
+gsap.to(".a", {
+  x: 250,
+  rotation: 0,
+  scrollTrigger: {
+    trigger: ".a",
+start: "top center",
+end: "top 100px",
+    scrub: true,
+    markers: true,
+    id: "scrub"
+  }
+  });
 gsap.to(".b", {
-  x: 400,
-  rotation: 360,
+    y: 950,
+  rotation: 0,
   scrollTrigger: {
     trigger: ".b",
-    start: "top center",
+  start: "top center",
     end: "top 100px",
     scrub: true,
     markers: true,
@@ -15,17 +27,28 @@ gsap.to(".b", {
   }
 });
 
-
-// Or you can attach a tween or timeline to a ScrollTrigger later
-const anim = gsap.to(".c", {
-  x: 400,
-  rotation: 360,
-  duration: 3
+gsap.fromTo(".c", {autoAlpha: 0}, {autoAlpha: 0.5, duration: 1} {
+    x: 250,
+  rotation: 0,
+  scrollTrigger: {
+    trigger: ".c",
+    start: "top center",
+    end: "top 100px",
+    scrub: true,
+    markers: true,
+    id: "scrub"
+  }
 });
+// Or you can attach a tween or timeline to a ScrollTrigger later
+  // const anim = gsap.to(".c", {
+    // x: 400,
+    // rotation: 360,
+    // duration: 3
+  // });
 
-ScrollTrigger.create({
-  trigger: ".c",
-  animation: anim,
+  // ScrollTrigger.create({
+    // trigger: ".c",
+  //   animation: anim,
   // Uncomment these to see how they affect the ScrollTrigger
   // markers: true,
   // start: "top center",
@@ -36,4 +59,4 @@ ScrollTrigger.create({
   // onUpdate: self => {
   //   console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
   // }
-});
+  // });
